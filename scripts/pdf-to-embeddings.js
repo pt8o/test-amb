ae13653c354814857ffae23915933f57794f0f3d
Lucas Huang <22176050+pt8o@users.noreply.github.com>
2023-07-07 11:32:02 -0400

    script to convert PDF to CSV with text + embeddings (#1)

---
+++ scripts/pdf-to-embeddings.js
@@ -0,0 +1,71 @@
+const pdfjs = require("pdfjs-dist");
+const { writeFileSync } = require("node:fs");
+const { Configuration, OpenAIApi } = require("openai");
+
+require("dotenv").config();
+
+const EMBEDDING_MODEL = "text-embedding-ada-002";
+// const COMPLETION_MODEL = "text-ada-001";
+
+if (process.argv.length === 2) {
+  console.log("Error: no path to PDF file provided");
+  return;
+}
+
+const filePath = process.argv[2];
+const fileName = filePath.split("\\").pop().split("/").pop();
+
+function arrayToCSV(data) {
+  return data.map((row) => row.join(",")).join("\n");
+}
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+pdfjs.getDocument(filePath).promise.then((doc) => {
+  const allPromises = Array.from({ length: doc.numPages }, (_, i) => i + 1).map(
+    (pageNumber) => {
+      return doc.getPage(pageNumber).then((page) => {
+        return page.getTextContent().then((rawText) => {
+          return rawText.items.map((item) => item.str).join(" ");
+        });
+      });
+    }
+  );
+
+  Promise.all(allPromises).then((vals) => {
+    openai
+      .createEmbedding({
+        model: EMBEDDING_MODEL,
+        input: vals,
+      })
+      .then((response) => {
+        const data = response.data.data;
+
+        const valsAndEmbeddings = vals.reduce(
+          (acc, currentValue, currentIndex) => {
+            return [
+              ...acc,
+              // wrap all contents in double quotes to prevent in-content commas from breaking CSV format
+              [
+                `"${currentValue}"`,
+                `"${JSON.stringify(data[currentIndex].embedding)}"`,
+              ],
+            ];
+          },
+          [["text", "embedding"]]
+        );
+
+        writeFileSync(
+          `app/assets/docs/${fileName}.embeddings.csv`,
+          arrayToCSV(valsAndEmbeddings),
+          "utf-8",
+          (err) => {
+            console.error("Error writing CSV file:", err);
+          }
+        );
+      });
+  });
+});
