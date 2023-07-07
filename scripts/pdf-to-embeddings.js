109af6f69e6101648b8298e211e3561eccec6368
Lucas Huang <hello@superko.org>
2023-07-07 09:42:55 -0400

    wip: script converts pdf to text and embeddings

--- scripts/pdf-to-embeddings.js
+++ scripts/pdf-to-embeddings.js
@@ -1,13 +1,28 @@
 const pdfjs = require("pdfjs-dist");
-const { readFileSync } = require("node:fs");
+const { writeFileSync } = require("node:fs");
+const { Configuration, OpenAIApi } = require("openai");
+
+require("dotenv").config()
+
+const EMBEDDING_MODEL = "text-embedding-ada-002";
+const COMPLETION_MODEL = "text-ada-001";
 
 if (process.argv.length === 2) {
   console.log("Error: no path to PDF file provided");
   return;
 }
 
+const filePath = process.argv[2];
+const fileName = filePath.split('\\').pop().split('/').pop();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
 pdfjs
-  .getDocument(process.argv[2])
+  .getDocument(filePath)
   .promise.then((doc) => {
     const allPromises = Array.from(
       { length: doc.numPages },
@@ -15,13 +30,25 @@ pdfjs
     ).map((pageNumber) => {
       return doc.getPage(pageNumber).then((page) => {
         return page.getTextContent().then((rawText) => {
-          return rawText.items.map((item) => item.str).join(" ");
+          return rawText.items.map((item) => item.str).join("\n");
         });
       });
     });
 
     Promise.all(allPromises).then((vals) => {
-      console.log(vals);
+      openai.createEmbedding({
+        model: EMBEDDING_MODEL,
+        input: vals
+      }).then(response => {
+        const data = response.data.data;
+        const valsAndEmbeddings = vals.reduce((acc, currentValue, currentIndex) => {
+          return [...acc, {
+            value: currentValue,
+            embedding: data[currentIndex].embedding
+          }]
+        }, []);
+        writeFileSync(`app/assets/docs/${fileName}.embedding.json`, JSON.stringify(valsAndEmbeddings));
+      }).catch((err) => console.log(err));
     });
   })
   .catch((err) => {
