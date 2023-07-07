9b6c0756524ae529486f62b77f7e3f62057e6d7b
Lucas Huang <hello@superko.org>
2023-07-07 11:21:04 -0400

    convert pdf to embeddings, save as csv

--- scripts/pdf-to-embeddings.js
+++ scripts/pdf-to-embeddings.js
@@ -2,10 +2,10 @@ const pdfjs = require("pdfjs-dist");
 const { writeFileSync } = require("node:fs");
 const { Configuration, OpenAIApi } = require("openai");
 
-require("dotenv").config()
+require("dotenv").config();
 
 const EMBEDDING_MODEL = "text-embedding-ada-002";
-const COMPLETION_MODEL = "text-ada-001";
+// const COMPLETION_MODEL = "text-ada-001";
 
 if (process.argv.length === 2) {
   console.log("Error: no path to PDF file provided");
@@ -13,44 +13,59 @@ if (process.argv.length === 2) {
 }
 
 const filePath = process.argv[2];
-const fileName = filePath.split('\\').pop().split('/').pop();
+const fileName = filePath.split("\\").pop().split("/").pop();
+
+function arrayToCSV(data) {
+  return data.map((row) => row.join(",")).join("\n");
+}
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
-pdfjs
-  .getDocument(filePath)
-  .promise.then((doc) => {
-    const allPromises = Array.from(
-      { length: doc.numPages },
-      (_, i) => i + 1
-    ).map((pageNumber) => {
+pdfjs.getDocument(filePath).promise.then((doc) => {
+  const allPromises = Array.from({ length: doc.numPages }, (_, i) => i + 1).map(
+    (pageNumber) => {
       return doc.getPage(pageNumber).then((page) => {
         return page.getTextContent().then((rawText) => {
-          return rawText.items.map((item) => item.str).join("\n");
+          return rawText.items.map((item) => item.str).join(" ");
         });
       });
-    });
+    }
+  );
 
-    Promise.all(allPromises).then((vals) => {
-      openai.createEmbedding({
+  Promise.all(allPromises).then((vals) => {
+    openai
+      .createEmbedding({
         model: EMBEDDING_MODEL,
-        input: vals
-      }).then(response => {
+        input: vals,
+      })
+      .then((response) => {
         const data = response.data.data;
-        const valsAndEmbeddings = vals.reduce((acc, currentValue, currentIndex) => {
-          return [...acc, {
-            value: currentValue,
-            embedding: data[currentIndex].embedding
-          }]
-        }, []);
-        writeFileSync(`app/assets/docs/${fileName}.embedding.json`, JSON.stringify(valsAndEmbeddings));
-      }).catch((err) => console.log(err));
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+
+        const valsAndEmbeddings = vals.reduce(
+          (acc, currentValue, currentIndex) => {
+            return [
+              ...acc,
+              // wrap all contents in double quotes to prevent in-content commas from breaking CSV format
+              [
+                `"${currentValue}"`,
+                `"${JSON.stringify(data[currentIndex].embedding)}"`,
+              ],
+            ];
+          },
+          [["text", "embedding"]]
+        );
+
+        writeFileSync(
+          `app/assets/docs/${fileName}.embeddings.csv`,
+          arrayToCSV(valsAndEmbeddings),
+          "utf-8",
+          (err) => {
+            console.error("Error writing CSV file:", err);
+          }
+        );
+      });
   });
+});
