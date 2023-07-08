9e53c05f85e851c9c63888e90fcf07e31a63f281
Lucas Huang <hello@superko.org>
2023-07-08 17:18:36 -0400

    cleanup

--- scripts/pdf-to-embeddings.js
+++ scripts/pdf-to-embeddings.js
@@ -5,7 +5,6 @@ const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
 const EMBEDDING_MODEL = "text-embedding-ada-002";
-// const COMPLETION_MODEL = "text-ada-001";
 
 if (process.argv.length === 2) {
   console.log("Error: no path to PDF file provided");
