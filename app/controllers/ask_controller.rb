a6ad344d838fab6ca4d468d6ff9a9ddf8bae004c
Lucas Huang <hello@superko.org>
2023-07-08 17:47:42 -0400

    refactor: make common openai_request method for future reuse

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -2,17 +2,33 @@ require 'uri'
 require 'net/http'
 require 'dotenv/load'
 
-OPENAI_COMPLETIONS_MODEL = 'gpt-3.5-turbo'.freeze
-
 class AskController < ApplicationController
+  OPENAI_API = {
+    'embeddings' => {
+      'url' => 'https://api.openai.com/v1/embeddings',
+      'model' => 'text-embedding-ada-002'
+    },
+    'completions' => {
+      'url' => 'https://api.openai.com/v1/chat/completions',
+      'model' => 'gpt-3.5-turbo'
+    }
+  }
+
   def create
     question = params[:question]
     response = get_new_answer
     render json: response, status: :created
   end
 
-  def get_new_answer
-    parsed_url = URI.parse('https://api.openai.com/v1/chat/completions')
+  private
+
+  def openapi_request(request_type, body)
+    return { 'error' => 'Invalid request type' } if OPENAI_API[request_type].empty?
+
+    url = OPENAI_API[request_type]['url']
+    model = OPENAI_API[request_type]['model']
+
+    parsed_url = URI.parse(url)
     http = Net::HTTP.new(parsed_url.host, parsed_url.port)
     http.use_ssl = true
 
@@ -20,14 +36,23 @@ class AskController < ApplicationController
     request['Content-Type'] = 'application/json'
     request['Accept'] = 'application/json'
     request['Authorization'] = "Bearer #{ENV['OPENAI_API_KEY']}"
-    request.body = {
-      'model' => OPENAI_COMPLETIONS_MODEL,
-      'messages' => [{ "role": 'user', "content": 'Say this is a test!' }],
-      'max_tokens' => 150,
-      'temperature' => 0.0
-    }.to_json
 
-    response = http.request(request)
+    complete_body = body
+    complete_body['model'] = model
+    request.body = complete_body.to_json
+
+    http.request(request)
+  end
+
+  def get_new_answer
+    response = openapi_request(
+      'completions',
+      {
+        'messages' => [{ "role": 'user', "content": 'Say this is a test!' }],
+        'max_tokens' => 150,
+        'temperature' => 0.0
+      }
+    )
 
     return { 'error' => response.body } unless response.is_a?(Net::HTTPSuccess)
 
