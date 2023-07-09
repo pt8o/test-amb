0b0b549e3fd1a8bc50e4469f460a44a923c17cc5
Lucas Huang <hello@superko.org>
2023-07-09 09:28:39 -0400

    fetch completions from openai

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -5,25 +5,14 @@ class AskController < ApplicationController
 
   def create
     question = params[:question]
-    response = get_new_answer
+    response = get_new_answer(question)
     render json: response, status: :created
   end
 
   private
 
-  def get_new_answer
-    response = openapi_request(
-      'completions',
-      {
-        'messages' => [{ "role": 'user', "content": 'Say this is a test!' }],
-        'max_tokens' => 150,
-        'temperature' => 0.0
-      }
-    )
-
-    return { 'error' => response.body } unless response.is_a?(Net::HTTPSuccess)
-
-    response_body = JSON.parse(response.body)
-    { 'message' => response_body['choices'][0]['message']['content'] }
+  def get_new_answer(question)
+    completion = get_completion(question)
+    { 'message' => completion }
   end
 end
--- lib/utils/openai_request.rb
+++ lib/utils/openai_request.rb
@@ -2,7 +2,11 @@ require 'dotenv/load'
 require 'uri'
 require 'net/http'
 
+require_relative './compare_embeddings'
+
 module OpenAiRequest
+  include CompareEmbeddings
+
   OPENAI_API = {
     'embeddings' => {
       'url' => 'https://api.openai.com/v1/embeddings',
@@ -14,7 +18,7 @@ module OpenAiRequest
     }
   }
 
-  def openapi_request(request_type, body)
+  def openai_request(request_type, body)
     return { 'error' => 'Invalid request type' } if OPENAI_API[request_type].empty?
 
     url = OPENAI_API[request_type]['url']
@@ -34,5 +38,48 @@ module OpenAiRequest
     request.body = complete_body.to_json
 
     http.request(request)
+  end
+
+  def get_embeddings(text)
+    response = openai_request('embeddings', {
+                                'input' => text
+                              })
+
+    response_parsed = JSON.parse(response.body)
+    response_parsed['data'][0]['embedding']
+  end
+
+  def get_completion(question)
+    sorted_texts = compare_embeddings(get_embeddings(question))
+
+    messages = [
+      {
+        'role' => 'system',
+        'content' => "You are an assistant answering questions about a book.\n
+          Please keep answers to three sentences maximum.\n
+          Answer questions solely based on the text provided, not based on your own outside knowledge.\n
+          If you don't know the answer to something, please say: 'Sorry, I don't know the answer to that based on this book's contents.'\n
+          If you don't understand a question, please say: 'Sorry, I don't understand the question.'\n
+          \n
+          The book is 'Stories of My Dogs' by Leo Tolstoy.\n
+          Here are some of the most pertinent passages:\n
+          #{sorted_texts.join("\n")}
+        "
+      },
+      {
+        'role' => 'user',
+        'content' => question
+      }
+    ]
+
+    response = openai_request('completions', {
+                                'max_tokens' => 150,
+                                'temperature' => 0.0,
+                                'messages' => messages
+                              })
+
+    completion_content = JSON.parse(response.body)
+    puts completion_content
+    completion_content['choices'][0]['message']['content']
   end
 end
