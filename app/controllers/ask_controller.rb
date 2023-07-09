955c8cf9e1b08f41693e8d7ce46cec1f2a3ac2a5
Lucas Huang <hello@superko.org>
2023-07-09 18:39:09 -0400

    save new questions, return db answers for existing questions

--- app/controllers/ask_controller.rb
+++ app/controllers/ask_controller.rb
@@ -3,7 +3,18 @@ class AskController < ApplicationController
 
   def create
     question = params[:question]
-    response = get_new_answer(question)
+    question_answer = QuestionAnswer.find_by(question:)
+
+    if question_answer
+      response = {
+        'message' => question_answer.answer
+      }
+    else
+      response = get_new_answer(question)
+      question_answer = QuestionAnswer.new(question:, answer: response['message'])
+      question_answer.save
+    end
+
     render json: response, status: :created
   end
 
