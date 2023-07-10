b93743ee9af2ca08ddfdcd31bcc25d6a361eaadb
Lucas Huang <hello@superko.org>
2023-07-10 07:31:23 -0400

    fix ask_controller_test

--- test/controllers/ask_controller_test.rb
+++ test/controllers/ask_controller_test.rb
@@ -1,10 +1,13 @@
+require 'rails_helper'
+require_relative '../../app/controllers/ask_controller'
+
 RSpec.describe AskController, type: :controller do
   describe 'POST #create' do
     describe 'new question' do
       let(:question) { 'This is a test' }
 
       before do
-        allow(controller).to receive(:get_new_answer).and_return('Confirmed, this is a test')
+        allow(controller).to receive(:get_completion).and_return({ 'message' => 'Confirmed, this is a test' })
       end
       it 'returns a successful response' do
         post :create, params: { question: }
@@ -16,8 +19,8 @@ RSpec.describe AskController, type: :controller do
         expect(JSON.parse(response.body)).to eq({ 'message' => 'Confirmed, this is a test' })
       end
 
-      it 'calls get_new_answer with the provided question' do
-        expect(controller).to receive(:get_new_answer).with(question)
+      it 'calls get_completion with the provided question' do
+        expect(controller).to receive(:get_completion).with(question)
         post :create, params: { question: }
       end
     end
@@ -35,8 +38,8 @@ RSpec.describe AskController, type: :controller do
         expect(JSON.parse(response.body)).to eq({ 'message' => 'An existing answer' })
       end
 
-      it 'does not call get_new_answer' do
-        expect(controller).not_to receive(:get_new_answer)
+      it 'does not call get_completion' do
+        expect(controller).not_to receive(:get_completion)
         post :create, params: { question: }
       end
     end
